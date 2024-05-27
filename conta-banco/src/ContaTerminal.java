b40a73172a22056f123887099b074b72339cb68f
thiago <arthi.thiago@gmail.com>
2024-05-27 15:41:02 -0300

    ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("************************");
+        System.out.print("Digite o número da conta: ");
+        numeroConta = scanner.nextInt();
+
+        System.out.println("************************");
+        System.out.print("Digite a agência: ");
+        agencia = scanner.next();
+
+        System.out.println("************************");
+        System.out.print("Digite seu nome: ");
+        nomeCliente = scanner.next();
+
+        System.out.println("************************");
+        System.out.print("Digite o saldo: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println();
+        System.out.println();
+        System.out.println("************************");
+        System.out.println("Dados da conta:");
+        
+        System.out.println("************************");
+        System.out.println("Número da conta: " + numeroConta);
+        
+        System.out.println("************************");
+        System.out.println("Agência: " + agencia);
+        
+        System.out.println("************************");
+        System.out.println("Nome do cliente: " + nomeCliente);
+        
+        System.out.println("************************");
+        System.out.println("Saldo: R$" + saldo);
+        
+        System.out.println("************************");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque. ");
     }
 }
